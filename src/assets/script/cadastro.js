a0e4c13020b090f5875a0e8ba9e63843d8b1b364
LUILSON DE JESUS BRANDÃO <ljbrandao@sga.pucminas.br>
2023-06-25 16:35:42 -0300

    Correção de redirecionamento cadastro.js
    Redirecionamento após verificar as regras

--- src/assets/script/cadastro.js
+++ src/assets/script/cadastro.js
@@ -13,126 +13,127 @@ menuOpener.addEventListener('click', () => {
 
 // regras do formulário
 
-let validador = {
-  handleSubmit:(event)=>{
-    event.preventDefault(); //para o comportamento padrão (enviar)
-    let send = true; //enviar o ormulário
+const validador = {
+  handleSubmit: (event) => {
+    event.preventDefault();
 
-    let inputs = form.querySelectorAll('input'); //pega todos inputs do form
+    const inputs = form.querySelectorAll('input');
+    let send = true;
 
     validador.clearErrors();
 
-  for(let i=0; i<inputs.length; i++) {
-    let input = inputs[i]; 
-    let check = validador.checkInput(input);
-    if(check !== true) {
-      send = false;
-      validador.showError(input, check);
-    } //se a função não retornar true exiba o erro
-   } //realiza um loop em cada um dos campos e verifica cada um individualmente
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i];
+      const check = validador.checkInput(input);
+      if (check !== true) {
+        send = false;
+        validador.showError(input, check);
+      }
+    }
 
-    //se enviar o formulário
-    if(send) {
-      let formData = {
+    if (send) {
+      const formData = {
         nome: document.querySelector('#nome').value,
         CRM: document.querySelector('#CRM').value,
         email: document.querySelector('#email').value,
         senha: document.querySelector('#senha').value,
-        };
-        let formDataJSON = JSON.stringify(formData);
-        localStorage.setItem('dadosFormulario', formDataJSON);
-      form.submit();
-      window.location.href = 'login.html'
+      };
 
-      }
+      const formDataJSON = JSON.stringify(formData);
+      localStorage.setItem('dadosFormulario', formDataJSON);
+
+      // Realize o redirecionamento para a página de login aqui
+      window.location.href = 'login.html';
+    }
   },
-  //função que verifica se tem alguma regra
-  checkInput:(input)=>{
-    let regras = input.getAttribute('data-regras');
-    if(regras !== null) {
-      regras = regras.split('|');
-      for(let k in regras) {
-        let rDatails = regras[k].split('=');
-        switch(rDatails[0]){
-            case 'required':
-              if(input.value === '') { 
-                return 'Este campo é obrigatório.';
+
+  checkInput: (input) => {
+    const regras = input.getAttribute('data-regras');
+    if (regras !== null) {
+      const regrasArray = regras.split('|');
+      for (const regra of regrasArray) {
+        const [tipo, valor] = regra.split('=');
+        switch (tipo) {
+          case 'required':
+            if (input.value === '') {
+              return 'Este campo é obrigatório.';
+            }
+            break;
+          case 'min':
+            if (input.value.length < parseInt(valor)) {
+              return `Este campo precisa ter no mínimo ${valor} caracteres.`;
+            }
+            break;
+          case 'email':
+            if (input.value !== '') {
+              const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              if (!regex.test(input.value.toLowerCase())) {
+                return 'E-mail digitado não é válido!';
               }
-            break;
-            case 'min':
-              if(input.value.length < rDatails[1]) {
-                return 'Este campo precisa ter no mínimo '+ rDatails[1] +' caracteres';
-              } //requisição de caracteres
-            break;
-            case 'email':
-              if(input.value !=''){
-                let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
-                if(!regex.test(input.value.toLowerCase())) {
-                  return 'E-mail digitado não é válido!';
-                }
-              } //veifica se foi digitado um email
+            }
             break;
         }
       }
     }
     return true;
   },
-  showError:(input, error) =>{
-    input.style.borderColor = '#FF0000'; //quando houver erro deixar a borda vermelha
 
-    let errorElement = document.createElement('div');
-    errorElement.classList.add('error');
-    errorElement.innerHTML = error; //exibe o texto
+  showError: (input, error) => {
+    input.style.borderColor = '#FF0000';
 
-    input.parentElement.insertBefore(errorElement, input.ElementSibling);
-
+    let errorElement = input.nextElementSibling;
+    if (errorElement && errorElement.classList.contains('error')) {
+      errorElement.innerHTML = error;
+    } else {
+      errorElement = document.createElement('div');
+      errorElement.classList.add('error');
+      errorElement.innerHTML = error;
+      input.parentNode.insertBefore(errorElement, input.nextElementSibling);
+    }
   },
-  clearErrors:()=>{
-    let inputs = form.querySelectorAll('input');
-    for(let i=0; i<inputs.length;i++) {
-      inputs[i].style = '';
+
+  clearErrors: () => {
+    const errorElements = document.querySelectorAll('.validador .error');
+    for (const errorElement of errorElements) {
+      errorElement.remove();
     }
 
-    let errorElement = document.querySelectorAll('.error');
-    for(let i=0; i<errorElement.length;i++) {
-      errorElement[i].remove(); //evita a duplicidade dos erros
-      // Redirecionar para a página de login após a limpeza de erros
-      window.location.href = 'login.html'
+    const inputs = document.querySelectorAll('.validador input');
+    for (const input of inputs) {
+      input.style.borderColor = '';
     }
-  }
+  },
 };
 
-let form = document.querySelector('.validador');
-//bloqueio no envio
+const form = document.querySelector('.validador');
 form.addEventListener('submit', validador.handleSubmit);
 
-//Banco local de Usuários (lcalStorage)
+let usuarios = localStorage.getItem('meusUsuarios');
+const formulario = document.querySelector('form');
 
-let usuarios = localStorage.getItem("meusUsuarios");
-const formulario = document.querySelector("form");
-
-if(usuarios){
+if (usuarios) {
   usuarios = JSON.parse(usuarios);
-} else{
+} else {
   usuarios = [];
 }
 
-console.log (usuarios);
+console.log(usuarios);
 
-formulario.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let novoUsuario = new Object();
-    novoUsuario.nome = this.nome.value;
-    novoUsuario.CRM = this.CRM.value;
-    novoUsuario.email = this.email.value;
-    novoUsuario.senha = this.senha.value;
-    
-    usuarios.push(novoUsuario);
+formulario.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const novoUsuario = {
+    nome: this.nome.value,
+    CRM: this.CRM.value,
+    email: this.email.value,
+    senha: this.senha.value,
+  };
 
-    this.reset();
+  usuarios.push(novoUsuario);
 
-    localStorage.setItem("meusUsuarios", JSON.stringify(usuarios));
-})
+  this.reset();
+
+  localStorage.setItem('meusUsuarios', JSON.stringify(usuarios));
+});
 
 // Mostrar/Ocultar senha
 
